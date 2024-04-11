988978e636570afba256eb9bcf2d5da504d04f1a
fatima Zubeda <108570670+zubedafatima@users.noreply.github.com>
2024-04-11 19:14:57 +0500

    carts.js page

--- react-beeswax/src/components/cart.js
+++ react-beeswax/src/components/cart.js
@@ -1,3 +1,19 @@
-export function Cart() {
-  return <h1>Cart</h1>;
+import { useState } from "react";
+import "../styles/cartStyle.css";
+export function Cart(prop) {
+  const [items, setItems] = useState([]);
+
+  return (
+    <>
+      <div className="cart-container">
+        <div className="division">
+          <div className="left-container">
+            <div className="top-div"></div>
+            <div className="bottom-div"></div>
+          </div>
+          <div className="right-container"></div>
+        </div>
+      </div>
+    </>
+  );
 }
